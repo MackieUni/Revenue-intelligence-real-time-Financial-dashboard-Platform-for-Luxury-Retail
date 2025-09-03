922e50250dd13d0bbee734204a2e8ee393dd8ff4
MackieUni <ic.rondon@uniandes.edu.co>
2025-09-03 09:50:18 -0400

    Initial commit: Coach Retail Analytics Dashboard

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CoachRetailAnalytics from "./CoachRetailAnalytics";
+
+function App() {
+  return (
+    <div className="App">
+      <CoachRetailAnalytics />
+    </div>
+  );
+}
+
+export default App;
